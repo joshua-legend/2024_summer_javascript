e5c874afd1e6b48616f91f82b14454bdad0c648c
Joshua <76941328+joshua-legend@users.noreply.github.com>
2024-07-19 14:59:24 +0900

    Add files via upload

---
+++ Day01/hello.js
@@ -0,0 +1,6 @@
+// window.alert("오늘 자바스크립트 배우는 날 ㄹㅇㅋㅋ");
+// window.console.log("주말에는 놀러갈거임~");
+// window.console.log("주말계획: 수업임 ");
+
+// console 폴더 만들고 index.html, index.js 만들고
+// alert 자바스크립트 설정완료!
---
+++ Day01/memory/index.js
@@ -0,0 +1,27 @@
+const name = "강사시치";
+const coffee = "두유라떼";
+
+// window.console.log(name);
+// window.console.log(coffee);
+// window.console.log("제 이름은 name 좋아하는 커피는 coffee 입니다.");
+// window.console.log(`제 이름은 ${name} 좋아하는 커피는 ${coffee} 입니다.`);
+
+// mbti, weekend, youtube
+// 제 mbti는 ~~이구요, 주말에 ~~하구요, 유튜브는 ~~를 볼거에요!
+// 콘솔로 보여주면 됨
+
+// const mbti = "entj";
+// const weekend = "주말수업";
+// const youtube = "고양이쇼츠";
+
+// window.console.log(`제 mbti는 ${mbti}이구요, 주말에 ${weekend}하구요, 유튜브는 ${youtube}를 볼거에요!`);
+
+//프롬프트로 당신의 핸드폰 기종은?
+//프롬프트로 당신의 PC 기종은?
+
+//콘솔로 당신이 쓰는 핸드폰은 ~~이고, 당신의 PC는 ~~이군요!
+
+const phone = window.prompt("님 폰 뭐임?");
+const pc = window.prompt("님 pc 뭐임?");
+
+window.console.log(`내 폰은 ${phone} 내 pc ${pc}`);
