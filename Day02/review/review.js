9f3686e531e27a6ffbb306abd9934575f972b4a3
Joshua <76941328+joshua-legend@users.noreply.github.com>
2024-07-22 14:53:13 +0900

    Add files via upload

---
+++ Day02/review/review.js
@@ -0,0 +1,12 @@
+window.console.log("축구를 했습니다");
+window.alert("아이스 아메리카노!");
+
+// 1. 변수 mbti를 만들고 본인의 mbti를 기억시키고
+// 콘솔로 나의 mbti는 ~~~ 입니다!
+const mbti = "ENTJ";
+window.console.log(`나의 mbti ${mbti} 입니다!`);
+
+// 2. 프롬프트로 기분이 어떤가요 물어보고
+// 콘솔로 기분이 ~~ 하시군요! 화이팅!
+const feeling = window.prompt("기분이 어떤가요?");
+window.console.log(`기분이 ${feeling}하시군요! 화이탱!`);
